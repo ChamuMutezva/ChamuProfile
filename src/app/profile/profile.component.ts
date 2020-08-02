232e3614c9907b47942e2c2426bfc0d91ab787c1
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-08-02 22:01:00 +0200

    feat: added current language that i am studying

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
- // selector: 'app-profile',
+  // selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-name = "Chamu Mutezva";
-languages: string[] = ["Html", "CSS", "Javascript", "Angular", "Jquery"];
+  name = "Chamu Mutezva";
+  languages: string[] = ["Html", "CSS", "Javascript", "Angular"];
+  current: string = "React"
   constructor() { }
 
   ngOnInit() {
